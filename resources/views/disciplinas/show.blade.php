f9e8158ffa8c3aae745ec012019f78d5379cb7ac
Frederico Murakawa <fredmurakawa@eerp.usp.br>
2018-08-30 12:08:43 -0300

    Issue 10 - Listar turmas cadastradas

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -1,3 +1,7 @@
 <h1>{{ $disciplina->titulo }}</h1>
 <p>{{ $disciplina->ementa }}</p>
 <a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Inserir Turma</a>
+@foreach ($disciplina->turmas as $turma)
+	{{ $turma->ministrante }}
+	{{ $turma->inicio }}
+@endforeach
